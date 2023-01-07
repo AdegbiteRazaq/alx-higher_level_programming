c9cb1e444f766ab3fe80ef2b46bdf53f04a08084
AdegbiteRazaq <adegbiterasaq101@gmail.com>
2023-01-07 06:45:41 -0800

    task 101

---
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -0,0 +1,11 @@
+#!/usr/bin/bash
+def remove_char_at(str, n):
+    j = 0
+    string = []
+    for i in str:
+        if j == n:
+            j += 1
+            continue
+        j += 1
+        string.append(i)
+    return ''.join(string)
